81aaac84a32e1e14385338890926f5deb54f5092
root <root@N-20L6PF15HU2D.nsn-intra.net>
2020-11-07 15:03:06 +0100

    No classes approach V1

--- Subject.py
+++
@@ -1,8 +0,0 @@
-import enum
-
-class Subject(enum.Enum):
-    Mathematics = 1
-    Physics = 2
-    Literature = 3
-    French = 4
-    Law = 4
--- task.py
+++ task.py
@@ -28,36 +28,120 @@
 #
 # Delete these comments before commit!
 # Good luck.
-from Student import Student
-from SchoolRegister import SchoolRegister
-from Subject import Subject
 import json
+import logging
 
-def dumper(obj):
-    try:
-        return obj.toJSON()
-    except:
-        return obj.__dict__
+# School / Class:
+
+
+def is_school_already_in_dict(_school: str):
+    if _school in schoolDB:
+        return True
+    return False
+
+
+def is_class_already_in_school(_class: str, _school: str):
+    if is_school_already_in_dict(_school):
+        if _class in schoolDB[_school]:
+            return True
+    return False
+
+
+def add_school(_school: str):
+    schoolDB[_school] = {}
+
+
+def add_class_for_school(_class: str, _school: str):
+    if not is_school_already_in_dict(_school):
+        add_school(_school)
+    schoolDB[_school][_class] = [{}]
+
+
+def get_class_in_school(_class: str, _school: str):
+    if is_class_already_in_school(_class, _school):
+        return schoolDB[_school][_class]
+    logging.warning(
+        "Such class or school does not exist in the data structure!")
+    return None
+
+# Student / Subject
+
+
+def is_student_in_class(_student: str, _class):
+    if _student in _class[0]:
+        logging.warning("Student %s is already in this class", _student)
+        return True
+    return False
+
+
+def add_student_to_class(_student: str, _class):
+    if not is_student_in_class(_student, _class):
+        _class[0][_student] = {}
+
+
+def does_subject_exist_for_student_in_class(_subject: str, _student: str, _class):
+    if _subject in _class[0][_student]:
+        logging.warning(
+            "Subject %s already exists for Student %s", _subject, _student)
+        return True
+    return False
+
+
+def add_subject_for_student_in_class(_subject: str, _student: str, _class):
+    if not does_subject_exist_for_student_in_class(_subject, _student, _class):
+        _class[0][_student][_subject] = []
+
+
+def get_subject_for_student_in_class(_subject: str, _student: str, _class):
+    if does_subject_exist_for_student_in_class(_subject, _student, _class):
+        return [_student, _subject, _class[0][_student][_subject]]
+
+def add_marks_for_subject(_subject, *_marksList):
+    _subject.extend(_marksList)
 
 if __name__ == '__main__':
-    student1 = Student("Michal", "Loska")
-    student2 = Student("Andrzej", "Duda")
+    schoolDB = {}
 
-    schoolRegister = SchoolRegister()
-    schoolRegister.add_student(student1)
-    schoolRegister.add_grade_for_subject(student1, Subject.French, 5)
-    schoolRegister.add_grade_for_subject(student1, Subject.French, 2)
-    schoolRegister.add_grade_for_subject(student1, Subject.Literature, 2)
-    schoolRegister.add_grade_for_subject(student1, Subject.Literature, 2)
-    schoolRegister.add_subject(student1, Subject.Literature, 2)
+    add_school("AGH")
+    add_class_for_school("1year", "AGH")
+    add_class_for_school("1year", "UJ")
+    agh_year1 = get_class_in_school("1year", "AGH")
+    add_student_to_class("Michal Loska", agh_year1)
+    add_subject_for_student_in_class("Physics", "Michal Loska", agh_year1)
+    add_subject_for_student_in_class("Maths", "Michal Loska", agh_year1)
     
-    # schoolRegister.for_student(student1).add_grade_for_student()
+    loska_physics = get_subject_for_student_in_class(
+        "Physics", "Michal Loska", agh_year1)
+    
+    add_marks_for_subject(loska_physics[2], 2)
+    add_marks_for_subject(loska_physics[2], 4, 5)
+    # print(loska_physics)
+    # schoolDB["AGH"] = {}
 
-    # schoolRegister.add_student(student2)
-    schoolRegister.add_grade_for_subject(student2, Subject.Law, 1)
-    schoolRegister.display_school_register()
-    # schoolRegister.export_register_to_JSON('data.json')
-    # # dumper(schoolRegister.studentRegister)
-    # with open("data.json", 'w') as fp:
-    #     json.dump(schoolRegister.studentRegister, fp, default=dumper, indent=2)
+    # schoolDB["AGH"]["1rok"] = [{}]
+    # agh_1rok = schoolDB["AGH"]["1rok"]
+    # agh_1rok[0]["Loska"] = {}
+    # agh_1rok[0]["Loska"]["c++"] = (2,)
+    # agh_1rok[0]["Kolecki"] = {}
+    # agh_1rok[0]["Kolecki"]["python"] = (3,)
+    # # for_school_and_class(agh_1rok)
 
+    # # print(agh_1rok)
+    # print(agh_1rok[0])
+    print(schoolDB)
+    # data = None
+
+    # with open('result.json', 'w') as fp:
+    #     json.dump(schoolDB, fp)
+
+    # with open('result.json') as json_file:
+    #     data = json.load(json_file)
+
+    # print(data)
+    # agh_1rok["loska"]["fizyja"] = 2
+    # schoolDB["AGH"]["1rok"] | LISTA -> | ["LOSKA"]["fizyka"] += (3,)
+    # schoolDB = ["AGH", {}]
+    # print(schoolDB.index(0))
+    # schoolDB = {}
+    # add_school("AGH")
+    # displaySchoolDB()
